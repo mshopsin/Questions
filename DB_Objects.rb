be099b89fac32b735ce301f3bd8e046dc0be8681
App Academy student <info@appacademy.io>
2013-03-28 18:03:24 -0700

    Needs to become object oriented

--- DB_Objects.rb
+++ DB_Objects.rb
@@ -12,8 +12,8 @@ class User
     @lname = row['lname']
     @is_instructor = row['is_instructor']
 
-    like(id)
-
+    # like(id)
+   #  puts "karma #{average_karma}"
   end
 
   def ask_question(title, body)
@@ -30,6 +30,11 @@ class User
                       VALUES (?,?);", id, question_id)
   end
 
+  def average_karma
+    $db.execute("SELECT avg(cnt) from(SELECT COUNT(*) AS cnt FROM question_likes WHERE user_id = ? GROUP BY question_id);",
+       @id).first[0]
+  end
+
   def create_reply
   end
 
@@ -37,9 +42,6 @@ class User
   end
 
 end
-
-
-
 
 class Question
   attr_accessor :student, :num_likes, :followers, :title
@@ -52,40 +54,79 @@ class Question
   end
 
   def num_likes
+    query = <<-SQL
+      SELECT COUNT(*) AS likes
+        FROM question_likes
+      WHERE question_likes.question_id = ?
+    SQL
 
+    $db.execute(query, @id).first['likes'];
   end
 
   def self.most_liked(n)
+    $db.execute("SELECT question_id, count(*) FROM question_likes GROUP BY question_id ORDER BY count(*) DESC LIMIT ?", n).map do |question|
+      question['question_id']
+    end
   end
 
-  def followers
+  def followers ## returns array of id's of followers
+    query = <<-SQL
+      SELECT follower_id
+          AS follower
+        FROM question_followers
+       WHERE question_id = ?
+    SQL
+
+    $db.execute(query, @id).map do |follower| follower['follower']
+    end
   end
 
   def self.most_followed(n)
+    query = <<-SQL
+      SELECT question_id, COUNT(*)
+        FROM question_followers
+    GROUP BY question_id
+    ORDER BY count(*) DESC LIMIT ?
+    SQL
+
+    $db.execute(query, n).map do |question| question['question_id']
+  end
+
+  def most_replies(n)
+    query = <<-SQL
+        SELECT prior_reply_id, COUNT(*)
+          FROM question_replies
+         WHERE question_id = ?
+      GROUP BY prior_reply_id
+      ORDER BY COUNT(*) DESC LIMIT ?;
+    SQL
+
+    $db.execute(query, @id, n).map do |question| question['prior_reply_id'] end
   end
 
 end
 
 $db.execute( "select * from question" ) do |row|
-  question = Question.new(row)
-  p question
+  p "Question ##{row['id']}: #{Question.new(row).num_likes}"
 end
+
 
 class QuestionFollowers
-  def self.most_followed
-
-  end
-
-end
+end ### WHAT DO WE DO WITH THIS GUY.
 
 class QuestionReplies
-  def insert_reply
+  #attr_reader
+  def replies
   end
 
   def self.most_replied
+    $db.execute("select prior_reply_id from question_replies GROUP BY prior_reply_id ORDER BY count(*) DESC LIMIT 1;").first['prior_reply_id']
   end
 
 end
+#
+# p QuestionReplies.most_replied
+
 
 class QuestionActions
 end
@@ -98,16 +139,16 @@ class QuestionLikes
     @question_id = row['question_id']
   end
 end
-
+#
 $db.execute( "select * from user" ) do |row|
   usr = User.new(row)
   p usr
 end
-
-$db.execute( "select * from question_likes" ) do |row|
-  like_obj = QuestionLikes.new(row)
-  p like_obj
-end
+#
+# $db.execute( "select * from question_likes" ) do |row|
+#   like_obj = QuestionLikes.new(row)
+#   p like_obj
+# end
 
 
 
--- data_insert.sql
+++ data_insert.sql
@@ -1,20 +1,73 @@
-INSERT INTO user ('fname','lname','is_instructor')
-     VALUES ('Dar','Gani',0),
-            ('Matt','Shop',0),
-            ('Kyle', 'L', 1);
+---- INSERT INTO user ('fname','lname','is_instructor')
+----      VALUES ('Kush','Patel',0),
+----       ('Ned','Ruggeri',1);
+----
+---- INSERT INTO question ('title','body','author_id')
+----      VALUES ('Who is leaving sleeping mats out?', 'Land Lord coming today', 2),
+----       ('JOBS', 'Who needs one?', 1);
 
 
-INSERT INTO question ('title','body','author_id')
-     VALUES ('Questions3', 'Body3', 3),
-            ('Questions4', 'Body4', 4),
-            ('Questions5', 'Body5', 4),
-            ('Questions6', 'Body6', 3);
+-- INSERT INTO user ('fname','lname','is_instructor')
+--      VALUES ('Dar','Gani',0),
+--             ('Matt','Shop',0),
+--             ('Kyle', 'L', 1);
+--
+--
+-- INSERT INTO question ('title','body','author_id')
+--      VALUES ('Questions3', 'Body3', 3),
+--             ('Questions4', 'Body4', 4),
+--             ('Questions5', 'Body5', 4),
+--             ('Questions6', 'Body6', 3);
+--
+-- INSERT INTO question_likes('user_id','question_id')
+--      VALUES (1, 4),
+--             (1, 5),
+--             (1, 6),
+--             (2, 4),
+--             (2, 2),
+--             (3, 4),
+--             (3, 1);
 
-INSERT INTO question_likes ('user_id','question_id')
-     VALUES (1, 4),
-            (1, 5),
-            (1, 6),
-            (2, 4),
-            (2, 2),
-            (3, 4),
-            (3, 1);
+-- DROP TABLE question_replies;
+--
+-- CREATE TABLE question_replies(
+--   id INTEGER PRIMARY KEY,
+--   question_id INTEGER NOT NULL,
+--   prior_reply_id INTEGER,
+--   user_id INTEGER,
+--   reply TEXT(1000),
+--
+--   FOREIGN KEY(user_id) REFERENCES user(id),
+--   FOREIGN KEY(question_id) REFERENCES question(id)
+-- );
+--
+-- DROP TABLE question_followers;
+--
+-- CREATE TABLE question_followers(
+--   id INTEGER PRIMARY KEY,
+--   follower_id INTEGER NOT NULL,
+--   question_id INTEGER NOT NULL,
+--
+--   FOREIGN KEY(follower_id) REFERENCES user(id),
+--   FOREIGN KEY(question_id) REFERENCES question(id)
+-- );
+--
+-- INSERT INTO question_followers('follower_id','question_id')
+--      VALUES (1, 1),
+--             (1, 2),
+--             (1, 3),
+--             (2, 4),
+--             (3, 5),
+--             (4, 5);
+--
+--
+-- INSERT INTO question_replies('question_id', 'prior_reply_id', 'user_id', 'reply')
+--      VALUES (1, NULL, 1, "reply to question 1"),
+--             (1, 1, 2,"reply to reply 1 question 1"),
+--             (1, 2, 4,"reply to reply 2 question 1"),
+--             (3, 2, 3,"reply to reply 2 question 1");
+
+INSERT INTO question_replies('question_id', 'prior_reply_id', 'user_id', 'reply')
+     VALUES (1, 2, 5, "reply to question 1 reply 1");
+
+
